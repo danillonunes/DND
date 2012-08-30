00b82620ab5d6ebaf90df08636acad7560f354a4
Danillo Nunes <git@danillonunes.net>
2012-08-30 15:31:49 -0300

    Start DN\Drupal\Settings class

---
+++ Settings.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DN\Drupal;
+
+/**
+ * Global settings definitions.
+ */
+class Settings
+{
+    /**
+     * Apply all the global settings.
+     */
+    public function apply() {
+    }
+}
